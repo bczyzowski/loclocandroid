068d13dc536ec7483ac2d792ebf29eea82d9eca0
Bartosz Czyżowski <Bartosz Czyowski>
2016-11-29 18:23:42 +0100

    Pierwszy commit

---
+++ app/src/main/java/com/bczyzowski/locator/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.bczyzowski.locator;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends Activity {
+
+    private Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        submit = (Button) findViewById(R.id.activity_login_submit_btn);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LocationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
