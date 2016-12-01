d51e742ede10f4bfce321080654a5a87a394ed71
bCzyz <bczyzowski96@gmail.com>
2016-12-01 11:34:04 +0100

    Podlaczenie inputow w loginactivity

--- app/src/main/java/com/bczyzowski/locator/LoginActivity.java
+++ app/src/main/java/com/bczyzowski/locator/LoginActivity.java
@@ -6,24 +6,39 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
-
+    private static final String TAG = "LoginActivity";
     private Button submit;
+    private TextView gotToSignup;
+    private EditText emailText, passwordText;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        submit = (Button) findViewById(R.id.activity_login_submit_btn);
+        submit = (Button) findViewById(R.id.btn_login);
+        gotToSignup = (TextView) findViewById(R.id.link_signup);
+        emailText = (EditText) findViewById(R.id.input_email);
+        passwordText = (EditText) findViewById(R.id.input_password);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), LocationActivity.class);
                 login();
+            }
+        });
+        gotToSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -32,9 +47,25 @@ public class LoginActivity extends Activity {
 
     }
 
+    @Override
+    protected void onDestroy() {
+        dismissProgressDialog();
+        super.onDestroy();
+    }
+
     private void login() {
 
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this, ProgressDialog.THEME_HOLO_LIGHT);
+        Log.d(TAG,"Login");
+
+        //checking input data
+        if (!validateInputData()) {
+            signupFailed();
+            return;
+        }
+
+        submit.setEnabled(false);
+
+        progressDialog = new ProgressDialog(LoginActivity.this, R.style.AppTheme_Dark_Dialog);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Authenticating ...");
         progressDialog.show();
@@ -44,11 +75,50 @@ public class LoginActivity extends Activity {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                progressDialog.dismiss();
+                signupSuccess();
+                dismissProgressDialog();
+                Intent intent = new Intent(getApplicationContext(), LocationActivity.class);
+                startActivity(intent);
+                finish();
             }
         };
 
         Handler progressDialogHandler = new Handler();
         progressDialogHandler.postDelayed(runnable, 3000);
+    }
+
+    private void signupSuccess() {
+        submit.setEnabled(true);
+    }
+
+    private void signupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+        submit.setEnabled(true);
+    }
+
+    private boolean validateInputData() {
+        boolean result = true;
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailText.setError("enter a valid email address");
+            result = false;
+        } else {
+            emailText.setError(null);
+        }
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            passwordText.setError("between 4 and 10 alphanumeric characters");
+            result = false;
+        } else {
+            passwordText.setError(null);
+        }
+        return result;
+    }
+
+    private void dismissProgressDialog() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
     }
 }
--- app/src/main/java/com/bczyzowski/locator/SignupActivity.java
+++ app/src/main/java/com/bczyzowski/locator/SignupActivity.java
@@ -1,13 +1,28 @@
 package com.bczyzowski.locator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignupActivity extends Activity {
+
+    private TextView goBackToLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        goBackToLogin = (TextView) findViewById(R.id.link_login);
+        goBackToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
